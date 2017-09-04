5fafb1334de8f0ce5f20d803badc55a99ffd025a
GrahamH68 <GrahamH68@hotmail.com>
2017-09-04 19:25:37 +0100

    Updated from Python 2 to Python 3

--- ex24.py
+++ ex24.py
@@ -1,5 +1,6 @@
-print "Let's practice everything."
-print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
+print("Let's practice everything.")
+print('You\'d need to know \'bout escapes with \\ that do:')
+print('\n newlines and \t tabs.')
 
 poem = """
 \tThe lovely world
@@ -10,13 +11,13 @@ and requires an explanation
 \n\t\twhere there is none.
 """
 
-print "--------------"
-print poem
-print "--------------"
+print("--------------")
+print(poem)
+print("--------------")
 
 
 five = 10 - 2 + 3 - 6
-print "This should be five: %s" % five
+print(f"This should be five: {five}")
 
 def secret_formula(started):
     jelly_beans = started * 500
@@ -28,10 +29,14 @@ def secret_formula(started):
 start_point = 10000
 beans, jars, crates = secret_formula(start_point)
 
-print "With a starting point of: %d" % start_point
-print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
+# remember that this is another way to format a string
+print("With a starting point of: {}".format(start_point))
+# it's just like with an f"" string
+print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 
 start_point = start_point / 10
 
-print "We ca also do that this way:"
-print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
+print("We can also do that this way:")
+formula = secret_formula(start_point)
+# this is an easy way to apply a list to a format string
+print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
